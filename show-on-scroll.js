689c277353f323a46e07b976cb5b80722a172743
fernandezfranz1216 <fernandezfranz1216@gmail.com>
2024-02-04 13:07:21 +0800

    Add project

--- show-on-scroll.js
+++ show-on-scroll.js
@@ -28,13 +28,11 @@ function isElementInViewport(el) {
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
+    (rect.top <= 0 && rect.bottom >= 0)
     ||
     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
       rect.top <= (window.innerHeight || document.documentElement.clientHeight))
     ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
